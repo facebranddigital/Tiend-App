2259c03c531449392f9527792d0d1bcec165a285
Yohon Jairo Bravo <yjairobravo@gmail.com>
2026-03-26 20:50:23 -0500

    :sparkles: add form modal and validations

--- src/app/pages/landing/landing.ts
+++ src/app/pages/landing/landing.ts
@@ -1,8 +1,10 @@
 import { Component, signal } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './landing.html',
   styleUrl: './landing.scss'
 })
@@ -10,11 +12,31 @@ export class LandingComponent {
   showModal = signal(false);
   showRegisterModal = signal(false);
 
+  registerForm = new FormGroup({
+    firstName: new FormControl('', [Validators.required]),
+    lastName: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    phone: new FormControl('', [Validators.required, Validators.pattern('^[0-9+ ]*$')])
+  });
+
   toggleModal() {
     this.showModal.update(v => !v);
   }
 
   toggleRegisterModal() {
+    if (!this.showRegisterModal()) {
+      this.registerForm.reset();
+    }
     this.showRegisterModal.update(v => !v);
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      console.log('Form Submitted', this.registerForm.value);
+      this.toggleRegisterModal();
+      alert('¡Registro exitoso! Bienvenido a TIEND.');
+    } else {
+      this.registerForm.markAllAsTouched();
+    }
   }
 }
