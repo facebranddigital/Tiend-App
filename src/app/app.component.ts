5f6abf52dc1c4e3c2e5147774d6e78c7dc36a380
facebranddigital-Darkarmadeus <facebranddigital@gmail.com>
2026-04-18 01:57:37 -0500

    fix: integración total y corrección de styleUrl para BracasBot

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 // 1. Importas el archivo que acabamos de crear
-import { ChatAiComponent } from './components/chat-ai/chat-ai.component';
-
+import { ChatAiComponent } from './components/chat-ai/chat-ai.component'; // 1. Importación física
 @Component({
   selector: 'app-root',
   standalone: true,
-  // 2. Lo agregas a la lista de imports aquí abajo
-  imports: [RouterOutlet, ChatAiComponent],
+  imports: [RouterOutlet, ChatAiComponent], // 2. Lo agregas a la lista de imports aquí abajo
   templateUrl: './app.component.html',
   styleUrl: './app.scss',
 })
