145e313952661adb2ef3a6d60512fa7675d21bff
facebranddigital-Darkarmadeus <facebranddigital@gmail.com>
2026-04-29 14:04:58 -0500

    Ajuste de logo centrado y botones 3D con brillo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,12 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyDHwKpocUR1xHtCdhabnNiEAOOQpaXR6Mk',
-    authDomain: 'tiend-app.firebaseapp.com',
-    projectId: 'tiend-app',
-    storageBucket: 'tiend-app.firebasestorage.app',
-    messagingSenderId: '441509358559',
-    appId: '1:441509350559:web:996a0f06a13a38b13a66aa',
-    measurementId: 'G-0XP0146XTK',
-  }, // <-- Esta cierra firebase
-  mercadoPagoPublicKey: 'TEST-1a7ef8d6-e414-4308-ba67-bd36d379818b',
-}; // <-- Esta cierra environment
+  "apiKey": "AIzaSyDHwKpocUR1xHtCdhabnNiEAOOQpaXR6Mk",
+  "authDomain": "tiend-app.firebaseapp.com",
+  "projectId": "tiend-app",
+  "storageBucket": "tiend-app.firebasestorage.app",
+  "messagingSenderId": "441509358559",
+  "appId": "1:441509350559:web:996a0f06a13a38b13a66aa",
+  "measurementId": "G-0XP0146XTK"
+}
+};
