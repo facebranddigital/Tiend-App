bf98c22d5c0af2cf5babe7342457830d3015ef07
facebranddigital-Darkarmadeus <facebranddigital@gmail.com>
2026-05-18 18:27:21 -0500

    style(landing): remover selectores duplicados de navbar y footer en las vistas individuales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common'; 
+import { CommonModule } from '@angular/common';
+
+// 1. Importamos los componentes apuntando a tus carpetas nativas
+import { NavbarComponent } from './components/navbar/navbar';
+import { FooterComponent } from './components/footer/footer';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     CommonModule,
-    RouterOutlet
+    RouterOutlet,
+    NavbarComponent, // 2. Registramos el Navbar para habilitar <app-navbar>
+    FooterComponent, // 3. Registramos el Footer para habilitar <app-footer>
   ],
   templateUrl: './app.component.html',
-  styleUrl: './app.scss',
+  styleUrl: './app.scss', // Mantenemos tu hoja de estilo original
 })
 export class AppComponent implements OnInit {
   title = 'Bracasfood';
